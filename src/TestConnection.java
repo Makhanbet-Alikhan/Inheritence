86d2bc97f11cf2b7667cd87479af7f972757f98a
Makhanbet-Alikhan <251160@astanait.edu.kz>
2026-01-31 13:21:03 +0500

    Delete src/TestConnection.java

--- src/TestConnection.java
+++
@@ -1,9 +0,0 @@
-import Database.ClothDAO;
-import model.Shirt;
-
-public class TestConnection {
-    public static void main(String[] args) {
-        ClothDAO dao = new ClothDAO();
-        dao.getAllpants();
-    }
-}
